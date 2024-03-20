c8e97f3e3d293a65e1dec883c393fe44281616e9
Mauricio <maugaspary@gmail.com>
2024-03-20 16:46:29 -0300

    Done karatsuba classic with strings

--- KaratsubaTriplo.java
+++ KaratsubaTriplo.java
@@ -1,32 +1,13 @@
-/*
- * The Karatsuba algorithm is a multiplication algorithm developed by Anatolii Alexeevitch Karatsuba in 1960.
- * It operates in O(n^log2(3)) time (~ O(n^1.585)), with n being the number of digits of the numbers we are multiplying together.
- * Standard grade-school multiplication operates in O(n^2) time. Karatsuba's method is asymptotically much faster.
- * Normally, you can choose any base you want, but we will be using base 10 in this algorithm with m varying depending on the length of the inputs.
- * Specific details are included with an example in the comments before the actual method.
- * 
- * @author Ayamin
- * 
- */
-
 public class KaratsubaTriplo {
 
-	// Takes two integers and returns the maximum of them
 	public static int max(int x, int y) {
 		return (x > y) ? x : y;
 	}
 
-	// Takes a string and an index.
-	// The index in this case is the "m". It will count backwards from the last
-	// (least significant) digit and split the string there.
-	// It will return a 2-element array of the split string.
-	// For example: Given 12345 as the string and 2 as the index, it will split the
-	// string into the string array ["123", "45"].
-	// This is so the 123 can be written as 123 * 10^m, with m = 2 the index.
-	public static String[] strCopy(long index, String string) {
+	public static String[] strCopy(int index, String string) {
 		String first = "",
 				last = "";
-		long actualIndex = string.length() - index;
+		int actualIndex = string.length() - index;
 		for (int i = 0; i < actualIndex; i++) {
 			first += string.charAt(i);
 		}
@@ -36,13 +17,11 @@ public class KaratsubaTriplo {
 		return new String[] { first, last };
 	}
 
-	// An exponent function. Works the same way as Math.pow, but with 64bit integers
-	// instead of double precision floats.
-	public static long power(long x, long y) {
+	public static int power(int x, int y) {
 		if (y == 0)
 			return 1;
 		else {
-			long answer = 1;
+			int answer = 1;
 			for (int i = 1; i <= y; i++) {
 				answer *= x;
 			}
@@ -50,88 +29,103 @@ public class KaratsubaTriplo {
 		}
 	}
 
-	/*
-	 * Take two numbers, x and y.
-	 * Example: 12345 and 6789.
-	 * Find a base b and power m to separate it into.
-	 * We'll pick base = 10, and m to be half the length of the digits of the
-	 * numbers in this implementation of the algorithm.
-	 * In this case, m will be 2, so 10^2 = 100. We will split the 2 numbers using
-	 * this multiplier.
-	 * The form we want is:
-	 * x = x1*b^m + x0
-	 * y = y1*b^m + y0
-	 * ----------
-	 * Using the above example,
-	 * x1 = 123
-	 * x0 = 45
-	 * ----------
-	 * y1 = 67
-	 * y2 = 89
-	 * ----------
-	 * b = 10 and m = 2
-	 * ----------
-	 * Thus:
-	 * 12345 = 123 * 10^2 + 45
-	 * 6789 = 67 * 10^2 + 89
-	 * 
-	 * 
-	 * The recursive algorithm is as follows:
-	 * 
-	 * If x<10 or y<10, return x*y. Single digit multiplication is the base case.
-	 * Otherwise:
-	 * Let z2 = karatsuba(x1, y1). x1 and y1 are the most significant digits, and
-	 * are the local variables "high".
-	 * Let z0 = karatsuba(x0, y0). x0 and y0 are the least significant digits, and
-	 * are the local variables "low".
-	 * Let z1 = karatsuba(x1+y0, x0+y1) - z0 - z2.
-	 * And the result is the following sum:
-	 * z2 * b^2m + z1 * b^m + z0
-	 * 
-	 * @param x The multiplicand.
-	 * 
-	 * @param y The multiplier.
-	 * 
-	 * @return The product.
-	 */
+	// public static String karatsuba(String x, String y, String z) {
+	// 	return null;
+
+	// }
 
 	public static String karatsuba(String x, String y) {
-		String xString = x;
-		String yString = y;
 		// Base case: single digit multiplication
-		if (xString.length() == 1 || yString.length() == 1) {
+		if (x.length() == 1 || y.length() == 1) {
 			int prod = Integer.parseInt(x) * Integer.parseInt(y);
 			return String.valueOf(prod);
+		
 		} else if (x.charAt(0) == '0' || y.charAt(0) == '0') {
-			// If one of the strings starts with zero, just multiply them normally
-			return karatsuba(x.substring(1), y.substring(1));
+		return karatsuba(x.substring(1), y.substring(1));
 		}
+
 		// Recursive case: Decompose the problem by splitting the integers and applying
 		// the algorithm on the parts.
-		else {
-			// Local variables
-			long m = max(xString.length(), yString.length()), // the maximum # of digits
-					m2 = m / 2; // the middle; if the number is odd, it will floor the fraction
-			String[] high1 = strCopy(m2, xString), // the most significant digits. this is the scalar multiplier for
-													// b^m2
-					low1 = strCopy(m2, xString), // the least significant digits. this is what is added on to high1*b^m2
-					high2 = strCopy(m2, yString), // same for y
-					low2 = strCopy(m2, yString), // same for y
-					// Three recursive calls
-					z0 = karatsuba(low1, low2), // z0 = x0y0
-					z2 = karatsuba(high1, high2), // z2 = x1y1
-					z1 = karatsuba((low1 + high1), (low2 + high2)) - z2 - z0; // z1 = (x0 + y1)*(x1 + y0) - z2 - z0,
-																				// courtesy of Karatsuba
 
-			return (z2 * power(10, 2 * m2) + (z1 * power(10, m2)) + z0);
+		else {
+			int m, m2;
+			m = max(x.length(), y.length());
+			m2 = m / 2;
+			String high1 = strCopy(m2, x)[0];
+			String low1 = strCopy(m2, x)[1];
+			String high2 = strCopy(m2, y)[0];
+			String low2 = strCopy(m2, y)[1];
+
+			String z0 = karatsuba(low1, low2);
+			String z2 = karatsuba(high1, high2);
+			String sumLowHigh1 = addStrings(low1, high1);
+        String sumLowHigh2 = addStrings(low2, high2);
+        
+        String z1 = subtractStrings(subtractStrings(karatsuba(sumLowHigh1, sumLowHigh2), z2), z0);
+        
+        return addStrings(addStrings(shiftLeft(z2, 2 * m2), shiftLeft(z1, m2)), z0);
+    }
+	}
+
+	public static String addStrings(String num1, String num2) {
+		StringBuilder result = new StringBuilder();
+		int carry = 0;
+		int i = num1.length() - 1;
+		int j = num2.length() - 1;
+	
+		while (i >= 0 || j >= 0 || carry > 0) {
+			int sum = carry;
+			if (i >= 0) {
+				sum += num1.charAt(i--) - '0';
+			}
+			if (j >= 0) {
+				sum += num2.charAt(j--) - '0';
+			}
+			carry = sum / 10;
+			result.insert(0, sum % 10);
 		}
+	
+		return result.toString();
+	}
+
+	public static String subtractStrings(String num1, String num2) {
+		StringBuilder result = new StringBuilder();
+		int borrow = 0;
+		int i = num1.length() - 1;
+		int j = num2.length() - 1;
+	
+		while (i >= 0 || j >= 0) {
+			int diff = borrow;
+			if (i >= 0) {
+				diff += num1.charAt(i--) - '0';
+			}
+			if (j >= 0) {
+				diff -= num2.charAt(j--) - '0';
+			}
+			if (diff < 0) {
+				diff += 10;
+				borrow = -1;
+			} else {
+				borrow = 0;
+			}
+			result.insert(0, diff);
+		}
+	
+		return result.toString();
+	}
+
+	public static String shiftLeft(String num, int digits) {
+		StringBuilder result = new StringBuilder(num);
+		for (int i = 0; i < digits; i++) {
+			result.append('0');
+		}
+		return result.toString();
 	}
 
 	public static void main(String[] args) {
 
-		// System.out.println(karatsuba(200, 200));
-		// System.out.println(karatsuba(12345, 6789));
-		// System.out.println(karatsuba(2358925, 1259174));
-		System.out.println(karatsuba(1234586789, 987654321));
+		//System.out.println(karatsuba("200", "200"));
+		System.out.println(karatsuba("20000", "200"));
+		//System.out.println(karatsuba("12432134341245674745675476", "7054920058988836008343024"));
 	}
 }
